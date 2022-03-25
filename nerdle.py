35a51435bbebade7bb27b30d3a7abe1d1e3e5144
josephwu273 <josephwu@buffalo.edu>
2022-03-25 14:50:58 -0400

    Total rewrite of guess/solution valiation code

--- nerdle.py
+++ nerdle.py
@@ -1,44 +1,81 @@
-from http.client import FORBIDDEN
-
-
 CHAR_SET = "0123456789+-*/="
-LEN = 8
-FORBIDDEN = ["//"]
+LENGTH = 8
+GUESS_FORBIDDEN = ["//"]
+SOLUTION_FORBIDDEN = ["**", "++", "+-", "-+", "--"] + GUESS_FORBIDDEN
+FIRST_FORBIDDEN = "0+-"
+NUM_EQUALS = 1
+
 
 class Guess:
-    def __init__(self, str):
-        self.f = str
+    @classmethod
+    def validate(cls, s):
+        le = cls.check_length(s)
+        ch = cls.check_chars(s)
+        ne = cls.check_numequals(s)
+        eq = cls.check_equality(s)
+        fi = cls.check_firstchar(s)
+        return le and ch and ne and eq
+
+    @staticmethod
+    def check_length(s):
+        """
+        Ensures <s> is the right LENGTH for a nerdle guess
+        """
+        return len(s)==LENGTH
+    
+    @staticmethod
+    def check_chars(s):
+        """
+        Checks that <s> has only allowed chars and no forbidden subsings
+        """
+        return all(c in CHAR_SET for c in s) and all(e not in s for e in GUESS_FORBIDDEN)
+    
+    @staticmethod
+    def check_numequals(s):
+        """
+        Checks <s> has the correct number of equals signs
+        """
+        return s.count("=")==NUM_EQUALS
+
+    @staticmethod
+    def split_equation(s):
+        return (s.split("=")[0], s.split("=")[1])
+    
+    @staticmethod
+    def check_equality(s):
+        """
+        Checks validity/equality of equation
+        """
+        #strip leading zeros because Python itself doesn't allow leading zeros
+        #fucking dumbass
+        s1, s2 = Guess.split_equation(s)
+        LHS =  ' '.join(str(int(x)) if x.isdigit() else x for x in s1.split())
+        RHS =  ' '.join(str(int(x)) if x.isdigit() else x for x in s2.split())
+        try:
+            return eval(LHS)==eval(RHS)
+        except:
+            return False
+    
+    @staticmethod
+    def check_firstchar(s):
+        return True
+
 
 class Solution(Guess):
-    def __init__(self, str):
-        self.f = str
+    @staticmethod
+    def check_chars(s):
+        g = Guess.check_chars(s)
+        return g and all(e not in s for e in SOLUTION_FORBIDDEN)
 
-def validate_guess(exp):
-    #Check input length
-    if len(exp) != 8:
-        return False
-    #Check every char is in CHAR_SET
-    if (not all(c in CHAR_SET for c in exp)):
-        return False
-    #Check exp does not contain "//"
-    if "//" in exp:
-        return False
-    #Check there is 1 and only 1 "="
-    eqn = exp.split("=")
-    if len(eqn) != 2:
-        return False
-    #Check LHS and RHS evaluate to the same
-    LHS = eqn[0]
-    RHS = eqn[1]
-    try:
-        x = eval(LHS)
-        y = eval(RHS)
-    except:
-        return False
-    return x==y
-
-def validate_solution(exp):
-    return "sadness"
+    @staticmethod
+    def check_firstchar(s):
+        LHS,RHS = Solution.split_equation(s)
+        l = all(LHS[0]!=c for c in FIRST_FORBIDDEN)
+        r = all(RHS[0]!=c for c in FIRST_FORBIDDEN)
+        return l and r
 
 
-print(validate_guess("4/2=+2"))
+
+
+print(Guess.validate("1**2=001"))
+print(Solution.validate("1**2=001"))
