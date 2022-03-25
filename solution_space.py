9d8c6e63328d0ebf3ee27e8e2aa0320fd990aca1
josephwu273 <josephwu@buffalo.edu>
2022-03-25 01:58:06 -0400

    umm

--- solution_space.py
+++ solution_space.py
@@ -1,10 +1,27 @@
-CHAR_SET = "0123456789+-*/="
+from http.client import FORBIDDEN
 
+
+CHAR_SET = "0123456789+-*/="
+LEN = 8
+FORBIDDEN = ["//"]
+
+class Guess:
+    def __init__(self, str):
+        self.f = str
+
+class Solution(Guess):
+    def __init__(self, str):
+        self.f = str
 
 def validate_guess(exp):
+    #Check input length
+    if len(exp) != 8:
+        return False
     #Check every char is in CHAR_SET
+    if (not all(c in CHAR_SET for c in exp)):
+        return False
     #Check exp does not contain "//"
-    if (not all(c in CHAR_SET for c in exp)) or "//" in exp:
+    if "//" in exp:
         return False
     #Check there is 1 and only 1 "="
     eqn = exp.split("=")
@@ -24,4 +41,4 @@ def validate_solution(exp):
     return "sadness"
 
 
-print(validate_guess("4//2=2"))
+print(validate_guess("4/2=+2"))
