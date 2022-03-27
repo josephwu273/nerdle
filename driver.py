4685a619321d81ab1bf5f3537ca6bb1530403e7d
josephwu273 <josephwu@buffalo.edu>
2022-03-27 18:14:45 -0400

    Improved runtime of driver by decreasing the number of strings to be considred

--- driver.py
+++ driver.py
@@ -7,18 +7,20 @@ from nerdle import *
 guess_space = []
 solution_space = []
 
-p = product(CHAR_SET,repeat=LENGTH)
-n = len(CHAR_SET)**LENGTH
+p = product("0123456789+-*/",repeat=LENGTH-1)
+n = 14**7 * 6
 i=0
 for s in p:
     s = "".join(s)
-    if Guess.validate(s):
-        guess_space.append(s)
-    if Solution.validate(s):
-        solution_space.append(s)
-    p = round(i/n * 100, 4)
-    print(f"{p}% done: Tabulating {i} of {n}", end="\r")
-    i += 1
+    for j in range(1,LENGTH-1):
+        q = s[:j]+"="+s[j:]
+        if Guess.validate(q):
+            guess_space.append(q)
+        if Solution.validate(q):
+            solution_space.append(q)
+        p = round(i/n * 100, 4)
+        i += 1
+        print(f"{p}% done", end="\r")
 
 print(len(guess_space))
 print(len(solution_space))
