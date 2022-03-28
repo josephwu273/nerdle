d2233b78d05605eb052756a4ce4602d106ed0224
josephwu273 <josephwu@buffalo.edu>
2022-03-28 00:16:23 -0400

    Added Timer class to driver

--- driver.py
+++ driver.py
@@ -1,7 +1,27 @@
 #WARNING: THIS CODE WILL TAKE A WHILE TO RUN
 from itertools import product
 from nerdle import *
+import time
+import datetime
+ 
 
+#code borrowed from https://code-maven.com/python-time-left
+class Timer(object):
+    def __init__(self, total):
+        self.start = datetime.datetime.now()
+        self.total = total
+ 
+    def remains(self, done):
+        now  = datetime.datetime.now()
+        #print(now-start)  # elapsed time
+        left = (self.total - done) * (now - self.start) / done
+        sec = int(left.total_seconds())
+        if sec < 60:
+           return f"{sec} seconds"
+        elif sec<3600:
+           return f"{int(sec/60)} minutes"
+        else:
+            return f"{int(sec/3600)} hours; {int((sec%3600)/60)} minutes"
 
 
 guess_space = []
@@ -10,6 +30,7 @@ solution_space = []
 p = product("0123456789+-*/",repeat=LENGTH-1)
 n = 14**7 * 6
 i=0
+t = Timer(n)
 for s in p:
     s = "".join(s)
     for j in range(1,LENGTH-1):
@@ -20,7 +41,7 @@ for s in p:
             solution_space.append(q)
         p = round(i/n * 100, 4)
         i += 1
-        print(f"{p}% done", end="\r")
+        print(f"{p}% done, {t.remains(i)}", end="\r")
 
 print(len(guess_space))
 print(len(solution_space))
