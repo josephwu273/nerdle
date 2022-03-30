f489087868622ca3070a642ac023afb9fbdf2f97
josephwu273 <josephwu@buffalo.edu>
2022-03-30 15:04:15 -0400

    Added finish method to show estimated finish time (versus time remaining)

--- Timer.py
+++ Timer.py
@@ -6,16 +6,25 @@ class Timer(object):
         self.start = datetime.datetime.now()
         self.total = total
         if disp:
-            print(f"Process started at {self.start}")
+            print(f" Process started at {self.start}")
+
+    def finish(self, done):
+        now = datetime.datetime.now()
+        left = (self.total - done) * (now - self.start) / done
+        finish = now+left
+        return f"{finish.strftime('%H:%M')}"
  
+    
     def remains(self, done):
         now  = datetime.datetime.now()
         #print(now-start)  # elapsed time
         left = (self.total - done) * (now - self.start) / done
         sec = int(left.total_seconds())
         if sec < 60:
-           return f"{sec} seconds"
+           return f"{sec}s"
         elif sec<3600:
-           return f"{int(sec/60)} minutes"
+           return f"{int(sec/60)}min"
         else:
-            return f"{int(sec/3600)} hours; {int((sec%3600)/60)} minutes"
+            h = sec//3600
+            m = (sec%3600)//60
+            return f"{h} hours, {m} minutes"
