b57703f12eb240d736dc45b215fceba995406801
josephwu273 <josephwu@buffalo.edu>
2022-03-30 03:42:34 -0400

    Lower-bounding solver.py

--- solver.py
+++ solver.py
@@ -1,31 +1,38 @@
 from nerdle import *
 from itertools import product
 from Timer import Timer
+from string import digits
+
 
 GUESS_FILE = "guess_space.txt"
 SOLUTION_FILE = "solution_space.txt"
 
 GUESS_SPACE = []
-with open(GUESS_FILE, "r") as gfile:
-    GUESS_SPACE = [e for e in gfile.read().split("\n") if e]
+try:
+    with open(GUESS_FILE, "r") as gfile:
+        GUESS_SPACE = [e for e in gfile.read().split("\n") if e]
+except:
+    pass
 SOLUTION_SPACE = []
-with open(SOLUTION_FILE, "r") as  sfile:
-    SOLUTION_SPACE = [s for s in sfile.read().split("\n") if s]
-
+try:
+    with open(SOLUTION_FILE, "r") as  sfile:
+        SOLUTION_SPACE = [s for s in sfile.read().split("\n") if s]
+except:
+    pass
 
 def generate_guess_space(disp=False):
     global GUESS_SPACE
-    if len(GUESS_SPACE)!=0:
-        raise Exception(f"{GUESS_FILE} is not empty. Clear it manually to make sure you are not overriding good data")
+    if len(GUESS_SPACE)!=0: pass
+        #raise Exception(f"{GUESS_FILE} is not empty. Clear it manually to make sure you are not overriding good data")
     gspace = []
     all_combos = product("0123456789+-*/",repeat=LENGTH-3)
-    n = 12*(14**5)*10 * 6
+    n = 12*(14**5)*1 * 6
     i=0
     t = Timer(n, disp)
     for c in all_combos:
         c = "".join(c)
         for x in "0123456789+-":
-            for y in "012345689":
+            for y in string.digits:
                 s = x+c+y
                 for j in range(1,LENGTH-1):
                     if s[j-1].isdigit() and (s[j] in "0123456789+-"):
@@ -37,7 +44,7 @@ def generate_guess_space(disp=False):
                         p = round(i/n*100, 4)
                         print(f"{eqn} {p}% done, {t.remains(i)}", end="\r")
     print()
-    with open(GUESS_FILE,"w") as gfile:
+    with open(GUESS_FILE,"a") as gfile:
         gfile.writelines(gspace)
     GUESS_SPACE = gspace
 
