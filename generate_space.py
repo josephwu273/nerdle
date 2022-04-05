fcfbe582976babf8f7f6772cdeef84c90ca8023b
josephwu273 <josephwu@buffalo.edu>
2022-04-05 00:56:14 -0400

    Moved space generation code to its own module

---
+++ generate_space.py
@@ -0,0 +1,88 @@
+from nerdle import *
+from itertools import product
+from Timer import Timer
+import string
+
+
+
+GUESS_FILE = "docs/guess_space.txt"
+SOLUTION_FILE = "docs/solution_space.txt"
+
+
+
+def get_file_contents(f):
+    with open(f, "r") as file:
+        contents = [e for e in file.read().split("\n") if e]
+    return contents
+
+
+def generate_guess_space(disp=True):
+    gspace = get_file_contents(GUESS_FILE)
+    if len(gspace)!=0:
+        raise Exception(f"{GUESS_FILE} is not empty. Clear it manually to make sure you are not overwriting good data")
+    all_combos = product(string.digits+"+-*/", repeat=LENGTH-3)
+    #n is the the total number of candidates we must check
+    #We peform some pruning first so n isn't actually isn't 14^LENGTH
+    n = 12*(14**5)*10*6
+    i=0
+    t = Timer(n, disp)
+    for c in all_combos:
+        c = "".join(c)
+        for x in string.digits+"+-":
+            #First entry cannot be * or / so prune those results...
+            for y in string.digits:
+                #Last entry must be a digit
+                s = x+c+y
+                for j in range(1,LENGTH-1):
+                    if s[j-1].isdigit() and (s[j] in (string.digits+"+-")):
+                        #Last entry of LHS must be a digit
+                        #First entry of RHS cannot be * or /
+                        eqn = s[:j]+"="+s[j:]
+                        if Guess.validate(eqn):
+                            gspace.append(eqn+"\n")
+                    if disp:
+                        i += 1
+                        p = round(i/n*100, 4)
+                        print(f" Finish in {t.remains(i)}; {p}% done", end="\r")
+    print()
+    print("Writing contents...")
+    with open(GUESS_FILE,"a") as gfile:
+        gfile.writelines(gspace)
+    return gspace
+
+
+def generate_solution_space(disp=True):
+    gspace = get_file_contents(GUESS_FILE)
+    if len(gspace)==0:
+        print("Generating guess space...")
+        gspace = generate_guess_space()
+    sspace = []
+    n = len(gspace)
+    i=0
+    t = Timer(n, disp)
+    for g in gspace:
+        if Solution.validate(g):
+            sspace.append(g+"\n")
+        if disp:
+            i += 1
+            p = round(i/n*100, 4)
+            print(f"{g} {p}% done, {t.remains(i)}", end="\r")
+    print()
+    print("Writing contents...")
+    with open(SOLUTION_FILE, "w") as sfile:
+        sfile.writelines(sspace)
+    return sspace
+
+if __name__=="__main__":
+    import sys
+    a = sys.argv[1]
+    if "g" in a and "s" in a:
+        print("GETTING GUESS AND SOLUTION SPACES...")
+        generate_guess_space()
+        generate_solution_space()
+    elif "g" in a and "s" not in a:
+        print("GETTING GUESS SPACE ONLY...")
+        generate_guess_space()
+    elif "g" not in a and "s" in a:
+        print("GETTING SOLUTION SPACE ONLY...")
+        generate_solution_space()
